7121da08bc17102c4dd81820533893eae2b23139
Allan Hipólito <allanhipolito66@gmail.com>
2024-04-20 16:03:23 -0300

    bugfix: alteracao url do login google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -52,7 +52,7 @@ function AuthProvider({children}:AuthProviderProps){
         try {
             const RESPONSE_TYPE = 'token';
             const SCOPE = encodeURI('profile email');
-            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
 
             const {type, params} = await AuthSession
             .startAsync({authUrl}) as AuthorizationResponse;
